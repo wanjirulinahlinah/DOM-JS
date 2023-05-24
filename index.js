0743ae4275bdff2be08b075a255d3ef375d3243c
wanjirulinahlinah <wainainalinahw@gmail.com>
2023-05-24 16:31:58 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementById('container').style.backgroundColor = 'silver'
+document.getElementById('title').style.color = 'green';
+document.getElementById('fruits').style.textTransform = 'Uppercase'
+document.getElementById('vegetables').style.textTransform = 'Uppercase'
+let add = document.createElement('fruList')
